4ddc46a9e88d7a410d28155381120870f1032d48
kayle <215031463@qq.com>
2017-09-04 00:34:27 +0800

    create CoreModule and SharedModule, and add SidebarComponent, HeaderComponent, FooterComponent in CoreModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MdSidenavModule } from '@angular/material';
+import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
